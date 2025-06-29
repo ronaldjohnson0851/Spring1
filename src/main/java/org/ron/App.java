8e7c1e59cf5f82ff29ea64fb6a235aae20c51c5d
RgJ3rd <137233800+RgJ3rd@users.noreply.github.com>
2025-06-28 21:23:14 -0400

    Updates

--- src/main/java/org/ron/App.java
+++ src/main/java/org/ron/App.java
@@ -1,18 +1,20 @@
 package org.ron;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj = (Alien) context.getBean("alien");
-        obj.code();
+        Alien obj1 = (Alien) context.getBean("alien1");
+        obj1.code();
+        //obj1.setAge(21);
+        System.out.println(obj1);
+
+
+//        Alien obj2 = (Alien) context.getBean("alien1");
+//        System.out.println(obj2.age);
+//        obj2.code();
     }
 }
---
+++ src/main/java/org/ron/Laptop.java
@@ -0,0 +1,11 @@
+package org.ron;
+
+public class Laptop {
+    public Laptop() {
+        System.out.println("Laptop Object Created");
+    }
+
+    public void compile(){
+        System.out.println("Compile");
+    }
+}
